147ad825120610eed9c2988c720cf5bbf1497560
Gio-69s <giovanni.adadja@gmail.com>
2025-08-05 19:49:50 +0000

    Refactor text generation code to remove unnecessary function and streamline user input loop

--- StudPal.py
+++ StudPal.py
@@ -5,24 +5,15 @@ task = "text-generation"
 model = "openai-community/gpt2"
 # Loading the model with a specified temperature for generation
 generator = pipeline(task, model , temperature= 0.7)
-def generate_text(prompt):
-    """
-    Generate text based on the provided prompt using the transformers pipeline.
-    
-    Args:
-        prompt (str): The input text to generate from.
-        
-    Returns:
-        str: The generated text.
-    """
+
 # Create a loop to continuously accept user input
-    while True:
-        prompt = input("You :")
-        # Generate text using the pipeline
-        response = generator(prompt.strip(), max_length=50, num_return_sequences=1)
-        return f"StudPal : {response[0]['generated_text']}"
-        # Check if the user wanna exit the loop
-        if prompt.lower() == 'exit':
-            print(" StudPal : See you soon champion!")
-            break
+while True:
+    prompt = input("You :")
+    # Generate text using the pipeline
+    response = generator(prompt.strip(), max_length=50, num_return_sequences=1)
+    print(f"StudPal : {response[0]['generated_text']}")
+    # Check if the user wanna exit the loop
+    if prompt.lower() == 'exit':
+        print(" StudPal : See you soon champion!")
+        break
         
